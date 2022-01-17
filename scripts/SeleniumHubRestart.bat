8e6116c8438b728fc7076082c4419106deac2f93
Nebojsa Borocki <nborocki@gmail.com>
2022-01-17 11:21:24 +0100

    Bump new driver, browsers and Moon versions

--- scripts/SeleniumHubRestart.bat
+++ scripts/SeleniumHubRestart.bat
@@ -1,9 +1,9 @@
 git pull
 
-SET CHROME=.\chromedriver\2.36\chromedriver.exe
-SET GECKO=.\geckodriver\0.27.0\geckodriver.exe
+SET CHROME=.\chromedriver\97.0.4692.71\chromedriver.exe
+SET GECKO=.\geckodriver\0.30.0\geckodriver.exe
 SET IE=.\iedriverserver\3.150.1\IEDriverServer.exe
-SET EDGE=.\MicrosoftWebDriver\87.0.664.66\msedgedriver.exe
+SET EDGE=.\MicrosoftWebDriver\97.0.1072.62\msedgedriver.exe
 SET SELENIUM=.\selenium-server-standalone\selenium-server-standalone-3.141.59.jar
 SET HUB=http://localhost:4444/grid/register
 
--- scripts/SetGlobalVariables.bat
+++ scripts/SetGlobalVariables.bat
@@ -1,6 +1,6 @@
-SET CHROME=.\chromedriver\93.0.4577.63\chromedriver.exe
+SET CHROME=.\chromedriver\97.0.4692.71\chromedriver.exe
 SET GECKO=.\geckodriver\0.30.0\geckodriver.exe
 SET IE=.\iedriverserver\3.150.1\IEDriverServer.exe
-SET EDGE=.\MicrosoftWebDriver\93.0.961.52\msedgedriver.exe
+SET EDGE=.\MicrosoftWebDriver\97.0.1072.62\msedgedriver.exe
 SET SELENIUM=.\selenium-server-standalone\selenium-server-standalone-3.141.59.jar
 SET HUB=http://HOSTNAME_or_IP_Address:4444/grid/register
