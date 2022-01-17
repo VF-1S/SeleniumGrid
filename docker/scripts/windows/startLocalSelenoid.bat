e3b7b5b347e0676c7e4a4903dbaa752a3c24738e
Nebojsa Borocki <nborocki@gmail.com>
2022-01-17 12:18:59 +0100

    Adding new browser version for Selenoid

--- docker/scripts/windows/startLocalSelenoid.bat
+++ docker/scripts/windows/startLocalSelenoid.bat
@@ -9,15 +9,18 @@ FOR /F "tokens=1* USEBACKQ" %%F IN (`docker images -a -q`) DO (
 )
 
 ::Download browser images
-docker pull selenoid/chrome:92.0
-docker pull selenoid/chrome:91.0
-docker pull selenoid/chrome:90.0
-docker pull selenoid/firefox:91.0
-docker pull selenoid/firefox:90.0
-docker pull selenoid/firefox:89.0
-docker pull browsers/edge:93.0
-docker pull browsers/edge:92.0
-docker pull selenoid/opera:77.0
+docker pull selenoid/chrome:97.0
+docker pull selenoid/chrome:96.0
+docker pull selenoid/chrome:95.0
+docker pull selenoid/firefox:96.0
+docker pull selenoid/firefox:95.0
+docker pull selenoid/firefox:94.0
+docker pull browsers/edge:97.0
+docker pull browsers/edge:96.0
+docker pull browsers/edge:95.0
+docker pull selenoid/opera:82.0
+docker pull selenoid/opera:81.0
+docker pull browsers/safari:15.0
 docker pull browsers/safari:14.0
 docker pull selenoid/video-recorder:latest-release 
 
